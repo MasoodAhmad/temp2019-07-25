71af2ab6d35c329f691cca6d1816f4ea2398f376
masoodahmad <masoodahmad>
2019-07-25 00:54:15 +0300

    third

--- src/main/java/parser1/App.java
+++ src/main/java/parser1/App.java
@@ -28,20 +28,60 @@ public class App {
         try (BufferedReader br = new BufferedReader(new FileReader("data/Participants.csv"))) {
             String line;
             while ((line = br.readLine()) != null) {
-                String[] values = line.split(DELIMITER);
-                String projectId = values[1];
-                String role = values[3];
+                String[] values     = line.split(DELIMITER);
+                String projectId    = values[1];
+                String role         = values[3];
                 if(role.equals("editor")){
                     int editorCount = (projects.get(projectId)).getNumber_of_participant_types().getEditor();
-                    (projects.get(projectId)).getNumber_of_participant_types().setEditor(editorCount+1);
+                    projects.get(projectId).getNumber_of_participant_types().setEditor(editorCount+1);
                 }
                 if(role.equals("writer")){
                     int writerCount = (projects.get(projectId)).getNumber_of_participant_types().getWriter();
-                    (projects.get(projectId)).getNumber_of_participant_types().setEditor(writerCount+1);
+                    projects.get(projectId).getNumber_of_participant_types().setWriter(writerCount+1);
                 }
                 if(role.equals("publisher")){
                     int publisherCount = (projects.get(projectId)).getNumber_of_participant_types().getPublisher();
-                    (projects.get(projectId)).getNumber_of_participant_types().setEditor(publisherCount+1);
+                    projects.get(projectId).getNumber_of_participant_types().setPublisher(publisherCount+1);
+                }
+            }
+        }catch (IOException ioe){
+            System.out.println(ioe);
+        }catch (Exception e){
+            System.out.println(e);
+        }
+
+        //Project Id,Keyword,Pending Task,State
+
+        try (BufferedReader br = new BufferedReader(new FileReader("data/Orders.csv"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values     = line.split(DELIMITER);
+                String projectId    = values[1];
+                String pendingTask  = values[3]; //writer task, Manager task etc
+                String state        = values[4]; // pending or not
+
+                if(state.equals("pending")){
+                    if(pendingTask.equals("WriterTask")){
+                        int writerTaskCount = (projects.get(projectId)).getNumber_of_pending_types().getWriterTask();
+                        projects.get(projectId).getNumber_of_pending_types().setWriterTask(writerTaskCount+1);
+                    }
+                    if(pendingTask.equals("WebSearchTask")){
+                        int webSearchTaskCount = (projects.get(projectId)).getNumber_of_pending_types().getWebSearchTask();
+                        projects.get(projectId).getNumber_of_pending_types().setWriterTask(webSearchTaskCount+1);
+                    }
+                    if(pendingTask.equals("EditorTask")){
+                        int editorTaskCount = (projects.get(projectId)).getNumber_of_pending_types().getEditorTask();
+                        projects.get(projectId).getNumber_of_pending_types().setWriterTask(editorTaskCount+1);
+                    }
+                    if(pendingTask.equals("ManagerTask")){
+                        int managerTaskCount = (projects.get(projectId)).getNumber_of_pending_types().getManagerTask();
+                        projects.get(projectId).getNumber_of_pending_types().setManagerTask(managerTaskCount+1);
+                    }
+                    if(pendingTask.equals("PublisherTask")){
+                        int publisherTaskCount = (projects.get(projectId)).getNumber_of_pending_types().getPublisherTask();
+                        projects.get(projectId).getNumber_of_pending_types().setPublisherTask(publisherTaskCount+1);
+                    }
+
                 }
             }
         }catch (IOException ioe){
